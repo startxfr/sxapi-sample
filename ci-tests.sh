7166c7c8408e8fb6b305ea7e60f296f503242074
startxfr <clarue@startx.fr>
2016-10-30 22:03:44 +0100

    change in CI tools

--- ci-tests.sh
+++ ci-tests.sh
@@ -6,9 +6,9 @@ mkdir /tmp/sxapi-sample;
 cd /tmp/sxapi-sample;
 git clone https://github.com/startxfr/sxapi-sample.git .
 echo "========> TESTING All containers"
-sudo docker-compose -f docker-compose.yml up -d
+sudo docker-compose -f samples/docker-compose.yml up -d
 echo "========> RESULT"
-sudo docker-compose -f docker-compose.yml ps
+sudo docker-compose -f samples/docker-compose.yml ps
 echo "=================> TEST ENDED SUCCESSFULLY"
 exit 0;
 
