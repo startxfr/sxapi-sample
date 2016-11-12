5372f16e5efe4cf8ad568ac00b6aebea99565405
startxfr <clarue@startx.fr>
2016-11-12 21:12:25 +0100

    bugfix sample websocket test

--- samples/websocket/test-ci.sh
+++ samples/websocket/test-ci.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-echo "=================> STARTING TEST v0.0.7"
-echo "=================> SETUP TEST ENVIRONMENT"
-set -ev
-mkdir /tmp/sxapi;
-cd /tmp/sxapi;
-git clone https://github.com/startxfr/sxapi-core.git .
-git checkout v0.0.7
-echo "========> BUILDING Service Container (v0.0.7)"
-sudo docker-compose build
-echo "========> TESTING OS Container (v0.0.7)"
-sudo docker-compose up -d
-echo "========> RESULT container (v0.0.7)"
-=======
 echo "=================> STARTING TEST OF YOUR SXAPI INSTANCE (v0.0.7)"
 echo "=================> SETUP TEST ENVIRONMENT (v0.0.7)"
 rm -rf /tmp/sxapi;
@@ -24,7 +10,6 @@ sudo docker-compose build
 echo "========> STARTING YOUR SXAPI INSTANCE (v0.0.7)"
 sudo docker-compose up -d
 echo "========> STATE OF YOUR SXAPI INSTANCE (v0.0.7)"
->>>>>>> master
 sudo docker-compose ps
 echo "=================> TEST ENDED SUCCESSFULLY"
 exit 0;
